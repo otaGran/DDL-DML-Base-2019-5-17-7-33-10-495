c01ba49f1e362c8e590872eab2b1bb951d05f1e5
otaGran <ujt2h8@gmail.com>
2019-07-03 22:34:52 +0800

    Update dml.sql

--- dml.sql
+++ dml.sql
@@ -1,7 +1,11 @@
 -- 插入记录  
+insert into student values ('001','李四',20,'女');
 
 -- 修改记录  
+update student set age=18 where id='001';
 
 --  删除记录  
+delete from student where id='001';
 
 -- 查询记录  
+select * from student;
